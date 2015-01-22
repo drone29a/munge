85bcf571f9ac2207991b24cf92c5485a99062031
Matt Revelle <mrevelle@gmail.com>
2015-01-22 11:14:05 -0500

    Updated to latest Clojure version.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
                  [org.clojure/data.csv "0.1.2"]
                  [net.mikera/core.matrix "0.32.2-SNAPSHOT"]
                  [net.mikera/vectorz-clj "0.28.1-SNAPSHOT"]
