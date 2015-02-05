9daa2e8a9b6e345b6367a2506ead6f39713d957c
Matt Revelle <mrevelle@gmail.com>
2015-02-04 19:06:58 -0500

    Moved over some convenience test methods.

--- test/munge/core_test.clj
+++ test/munge/core_test.clj
@@ -2,3 +2,9 @@
   (:require [clojure.test :refer :all]
             [munge.core :refer :all]))
 
+(defn close? [delta-thresh expected actual]
+  (< (Math/abs (- expected actual)) delta-thresh))
+
+(defn close-seq? [delta-thresh expected actual]
+  (every? (fn [[e a]] (< (Math/abs (- e a)) delta-thresh))
+          (map vector expected actual)))
