5d900be21cfe00bbed3d04990fe345392154fc50
Matt Revelle <mrevelle@gmail.com>
2015-07-20 17:16:50 -0400

    Updated deps and modified project info.

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject munge "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Support for data processing and anaylsis."
+  :url "https://github.com/mattrepl/munge"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
-                 [org.clojure/core.typed "0.3.0-alpha2"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.typed "0.3.7"]
                  [org.clojure/data.csv "0.1.2"]
                  [net.mikera/core.matrix "0.34.1-SNAPSHOT"]
                  [net.mikera/vectorz-clj "0.29.1-SNAPSHOT"]
-                 [prismatic/schema "0.4.1"]
+                 [prismatic/schema "0.4.3"]
                  [aysylu/loom "0.5.1-SNAPSHOT"]
                  [stemmers "0.2.2"]])
