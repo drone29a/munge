a5159e4652c37f75290228129d9f1a866cd06c96
Matt Revelle <mrevelle@gmail.com>
2015-07-29 15:01:13 -0400

    Removed snapshot deps.

--- project.clj
+++ project.clj
@@ -6,10 +6,10 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.typed "0.3.7"]
                  [org.clojure/data.csv "0.1.2"]
-                 [net.mikera/core.matrix "0.34.1-SNAPSHOT"]
-                 [net.mikera/vectorz-clj "0.29.1-SNAPSHOT"]
+                 [net.mikera/core.matrix "0.36.1"]
+                 [net.mikera/vectorz-clj "0.30.1"]
                  [prismatic/schema "0.4.3"]
-                 [aysylu/loom "0.5.1-SNAPSHOT"]
+                 [aysylu/loom "0.5.4"]
                  [stemmers "0.2.2"]
                  ;; TODO: Move to separate lib?
                  ;; [mkobos/lbfgsb-wrapper "1.1.3-SNAPSHOT" :native-prefix ""]
