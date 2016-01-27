0fe615368e3796ecf40c0e144ff71bd4d7319315
Matt Revelle <mrevelle@gmail.com>
2016-01-27 11:16:07 -0500

    Updated dependency versions.

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/mattrepl/munge"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0-RC4"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.typed "0.3.18"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [org.clojure/data.csv "0.1.2"]
-                 [net.mikera/core.matrix "0.45.0"]
-                 [net.mikera/vectorz-clj "0.37.0"]
+                 [net.mikera/core.matrix "0.49.0"]
+                 [net.mikera/vectorz-clj "0.42.0"]
                  [prismatic/schema "1.0.3"]
                  [aysylu/loom "0.5.4"]
                  [stemmers "0.2.2"]
