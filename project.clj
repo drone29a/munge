a63bf1f19f975f41cf251594852521d42fcd0d34
Matt Revelle <mrevelle@gmail.com>
2016-05-02 12:38:12 -0400

    Update data.csv version.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.typed "0.3.18"]
                  [org.clojure/core.match "0.3.0-alpha4"]
-                 [org.clojure/data.csv "0.1.2"]
+                 [org.clojure/data.csv "0.1.3"]
                  [net.mikera/core.matrix "0.49.0"]
                  [net.mikera/vectorz-clj "0.42.0"]
                  [prismatic/schema "1.0.3"]
